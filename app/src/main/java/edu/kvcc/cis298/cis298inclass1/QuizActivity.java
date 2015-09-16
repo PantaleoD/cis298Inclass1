a31f1c18a8f900c23d49ecd71de505c98626e87d
unknown <dpantaleo@ttc-2160-20.kvcc.dom>
2015-09-16 16:26:15 -0400

    finished chpt 1

--- app/src/main/java/edu/kvcc/cis298/cis298inclass1/QuizActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298inclass1/QuizActivity.java
@@ -4,13 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+       mTrueButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+
+           }
+       });
+
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() // we write the things to override   View class has OnClickListener and returns an instance of OnClickListener
+                                                                    // this allows us to not create our own....
+            //View.OnClickListener() creates a instnace of on click listener...with an instance of itself
+            //   and then override the onClick method within the OnClickListener
+        {
+            @Override
+            public void onClick(View v) {   // this doesn't do anything yet... so passing in googles class and overriding what you want
+           // now to do something:
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                //    method returns object it belongs to so can call another method...show
+            }
+        } );
     }
 
     @Override
